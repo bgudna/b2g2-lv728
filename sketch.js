aa40afe4a36729aadb4a4ec9fff35aa6c77e2f80
bgudna <b@bgudna.com>
2020-07-03 13:28:55 +0200

    random walking implemented

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 function setup() {
     createCanvas(640, 360);
     background(195,31,49);
+    pointer = new Point(100, 100);
 }
 
+
 function draw() {
-    
+    pointer.render();
+    pointer.step();
 }
